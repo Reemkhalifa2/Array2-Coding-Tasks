16ca64fe2bb38d84f97d0fd40b05c47b04f1bde0
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-08 10:53:43 +0400

    add task 4: 

---
+++ src/fizzArray.java
@@ -0,0 +1,9 @@
+public class fizzArray {
+    public static Integer[] fizzArray(Integer n){
+        Integer[] nums = new Integer[n];
+        for(Integer i=0; i<n; i++){
+            nums[i]=i;
+        }
+        return nums;
+    }
+}
