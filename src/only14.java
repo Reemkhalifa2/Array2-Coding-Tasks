75297befcbeb2e4f8b79cb276f3bf6e041c133a8
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-08 14:37:18 +0400

    add task 16: only14

---
+++ src/only14.java
@@ -0,0 +1,14 @@
+import com.sun.source.tree.BreakTree;
+
+public class only14 {
+    public static Boolean only14(Integer [] nums){
+        Boolean found = true;
+        for(Integer num:nums){
+            if(!(num == 4 || num == 1)){
+                found = false;
+            }
+        }
+
+        return found;
+    }
+}
