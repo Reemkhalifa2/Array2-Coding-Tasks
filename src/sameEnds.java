7505de2656467f5e9939cb0d624dddfc429c06ec
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-08 11:43:58 +0400

    add task 8 : sameEnds

---
+++ src/sameEnds.java
@@ -0,0 +1,11 @@
+public class sameEnds {
+    public static boolean sameEnds(int[] nums, int n) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != nums[nums.length - n + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
