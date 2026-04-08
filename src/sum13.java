59f08a64981fb01fe45db2675e8bb7235d1a93c0
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-08 10:22:19 +0400

    add task 2: sum13

--- src/sum13.java
+++ src/sum13.java
@@ -1,7 +1,7 @@
 public class sum13 {
     public Integer sum13(Integer[] nums){
-        int sum =0;
-        for(int i =0; i<nums.length; i++){
+        Integer sum =0;
+        for(Integer i =0; i<nums.length; i++){
              if (nums[i]==13 ) {
                  i++;
              }else{sum +=nums[i];}
