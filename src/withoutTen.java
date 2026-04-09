ca53c945f1db93dbab68f11dadb3d91702ae3077
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-09 11:03:57 +0400

    add task 11: withoutTen

--- src/withoutTen.java
+++ src/withoutTen.java
@@ -1,13 +1,13 @@
 public class withoutTen {
-    public static Integer[] withoutTen(Integer [] nums){
-            int index = 0;
-            for (int i = 0; i < nums.length; i++) {
+    public static Integer[] (Integer [] nums){
+            Integer index = 0;
+            for (Integer i = 0; i < nums.length; i++) {
                 if (nums[i] != 10) {
                     nums[index] = nums[i];
                     index++;
                 }
             }
-            for (int i = index; i < nums.length; i++) {
+            for (Integer i = index; i < nums.length; i++) {
                 nums[i] = 0;
             }
 
