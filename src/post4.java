372f6a0fa49ffc004a3fe84493b222870dde3e98
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-09 10:07:51 +0400

    add task 10 : post4

--- src/post4.java
+++ src/post4.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class post4 {
     public static Integer[] post4(Integer[] nums) {
-        Integer four = -1;
-        for (Integer i = 0; i < nums.length; i++) {
-            if (nums[i] == 4) {
-                four = i;
-            }
-        }
-
-        Integer size = nums.length - four - 1;
-        Integer[] result = new Integer[size];
-        for (Integer i = 0; i < size; i++) {
-            result[i] = nums[four + 1 + i];
-        }
-
-        return result;
+        Integer i = nums.length - 1;
+        while(nums[i] != 4)
+            i--;
+        Integer[] a = new Integer[nums.length - i - 1];
+        for(Integer j = 0; j < a.length; j++)
+            a[j] = nums[i + j + 1];
+        return a;
     }
 }
